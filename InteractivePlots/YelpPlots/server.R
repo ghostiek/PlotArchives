0bb4cba97c3923621e30d2f3570681d36d79ee81
ghostiek <patricktawil1@hotmail.com>
2018-02-25 17:14:24 -0800

    .

--- InteractivePlots/YelpPlots/server.R
+++ InteractivePlots/YelpPlots/server.R
@@ -10,14 +10,18 @@ business_info <- lapply(1:length(business), function(x) unlist(business[[x]][c(1
 shinyServer(
   function(input, output){
     output$plot1 <- renderPlot({
+      plotDens()
+      })
+    output$info <- renderText({
+      paste0("Number of reviews =", input$plot_click$x, "\nDensity =", input$plot_click$y)
+    })
+    
+    plotDens <- function(){
       ggplot(business_info[business_info$stars==input$slide,], aes(as.numeric(business_info[business_info$stars==input$slide,]$review_count))) +
         geom_density() +
         xlim(input$xmin, input$xmax) +
         xlab("Number of Reviews") +
         ggtitle(paste("Numer of Reviews vs.", input$slide, "Star Rating"))
-      })
-    output$info <- renderText({
-      paste0("Number of reviews =", input$plot_click$x, "\nDensity =", input$plot_click$y)
-    })
+    }
   }
 )
